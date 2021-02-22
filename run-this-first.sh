c801a9890761e3ac70807cd8f89f3333107ec527
PerfumedGoose82 <79411991+PerfumedGoose82@users.noreply.github.com>
2021-02-21 20:14:52 -0600

    Made terminal look better and fixed some spelling

--- run-this-first.sh
+++ run-this-first.sh
@@ -15,7 +15,8 @@ sleep 2
 open https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html
 fi
 
-read -p "Do you want a Vanilla minecraft server? " -n 1 -r
+clear
+read -p "Do you want a Vanilla minecraft server? If so, you will not have plugins. " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
@@ -23,18 +24,21 @@ then
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
+	clear
 	echo "Save the downloaded file into the new folder in your desktop"
 	sleep 1
 	open https://www.minecraft.net/en-us/download/server/
 	clear
         sh ~/Desktop/Minecraft\ Server/first-start-1.sh
 	fi
+	clear
         echo "To get a specific version of a vanilla minecraft server open Minecraft launcher, click Installations, click New, then select which version you want and above the box with the minecraft version there should be text that says server, then click it and it will start downloading in your web browser."
-	echo "Once downloaded, place it in the new folder in youe desktop"
+	echo "Once downloaded, place it in the new folder in your desktop"
 	clear
 	sh ~/Desktop/Minecraft\ Server/first-start-1.sh 
 fi
 
+clear
 echo "You will be redirected to download spigot, once there select the version on minecraft you want and place the downloaded file in the new folder in your desktop."
 sleep 4
 open https://getbukkit.org/download/spigot
